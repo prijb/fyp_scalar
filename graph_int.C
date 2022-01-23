aecbc9e678b6ecb8f0e727efd0672e613b3c3542
prijith <prijith.b.pradeep@gmail.com>
2022-01-23 23:06:38 +0000

    Numerical integration of rho and j

--- graph_int.C
+++ graph_int.C
@@ -1,18 +1,20 @@
 #include <vector>
 
-double int_rho(double pos){
+double int_rho(double *xvals, double *params){
 	
-	double t = 5;
-        double sig_p = 2.;
-        double m = 1;
+	double pos = xvals[0];
+	double t = params[0];
+        double sig_p = params[1];
+        double m = params[2];
 	double pi = TMath::Pi();
+
 	TF2* rho = new TF2("rho", "(0.25/pi)*(sqrt(2./pi)*(1/[1]))*(1 + [3]*[3]/(sqrt([3]*[3] + x*x)*sqrt([3]*[3] + y*y)) + x*y/(sqrt([3]*[3] + x*x)*sqrt([3]*[3] + y*y)))*cos((x-y)*[0] - (sqrt([3]*[3] + x*x) - sqrt([3]*[3] + y*y))*[2])*TMath::Exp(-1.*(x*x + y*y)/([1]*[1])) ", -100*sig_p, 100*sig_p, -100*sig_p, 100*sig_p);
 	rho->SetParameter(0,pos);
 	rho->SetParameter(1,sig_p);
 	rho->SetParameter(2,t);
 	rho->SetParameter(3,m);
 	
-	int  n_points = 500;
+	int  n_points = params[3];
 	double px_start = -10*sig_p;
 	double px_end = 10*sig_p;
 	double py_start = -10*sig_p;
@@ -55,19 +57,21 @@ double int_rho(double pos){
 	return dp*dp*intg;
 }
 
-double int_j(double pos){
+double int_j(double *xvals, double *params){
 
-        double t = 5;
-        double sig_p = 2.;
-        double m = 1;
+	double pos = xvals[0];
+        double t = params[0];
+        double sig_p = params[1];
+        double m = params[2];
         double pi = TMath::Pi();
+
         TF2* j = new TF2("j", "(0.25/pi)*(sqrt(2./pi)*(1/[1]))*(x/sqrt([3]*[3] + x*x) + y/sqrt([3]*[3] + y*y))*cos((x-y)*[0] - (sqrt([3]*[3] + x*x) - sqrt([3]*[3] + y*y))*[2])*TMath::Exp(-1.*(x*x + y*y)/([1]*[1])) ", -100*sig_p, 100*sig_p, -100*sig_p, 100*sig_p);
         j->SetParameter(0,pos);
         j->SetParameter(1,sig_p);
         j->SetParameter(2,t);
         j->SetParameter(3,m);
 
-        int  n_points = 500;
+        int  n_points = params[3];
         double px_start = -10*sig_p;
         double px_end = 10*sig_p;
         double py_start = -10*sig_p;
@@ -112,24 +116,30 @@ double int_j(double pos){
 
 void graph_int(){
 
-	//double pos = 0;
-	double t = 5;
-	double sig_p = 10.;
+	double t = 10.;
+	double sig_p = 1.;
 	double sig_x = 1.;
 	//double sig_x = 1./sig_p;
 	double m = 1;
-	double pi = TMath::Pi();
+	int n_points = 1000;
 
-	double params[] = {t,sig_p,m};
-
-	TF1* p_dens = new TF1("p_dens","int_rho(x)",-10*sig_x,10*sig_x);
-	TF1* j_dens = new TF1("j_dens","int_j(x)",-10*sig_x,10*sig_x);
+	double lim = 20*sig_x;
 	
+	TF1* p_dens = new TF1("p_dens",int_rho,-lim,lim,4);
+        TF1* j_dens = new TF1("j_dens",int_j,-lim,lim,4);
+
+	p_dens->SetParameters(t,sig_p,m,n_points);
+	j_dens->SetParameters(t,sig_p,m,n_points);
+
 	j_dens->SetLineColor(1);
 
-	double p_int = p_dens->Integral(-10*sig_x,10*sig_x,1.e-3);
-	double j_int = j_dens->Integral(-10*sig_x,10*sig_x,1.e-3);
-		
+	double p_int = p_dens->Integral(-lim,lim,1.e-3);
+	double j_int = j_dens->Integral(-lim,lim,1.e-3);
+	
+	std::ostringstream title;
+	title << std::fixed << std::setprecision(2) << "Comparison of probability density with current for a Gaussian (sig=" << 1/sig_p << ",m=" << m << ",t=" << t << ")";
+	std::string title_graph = title.str();	
+	
 	cout << "The integral of probability density is:  " << p_int << endl;
 
 	
@@ -139,20 +149,18 @@ void graph_int(){
         TMultiGraph* mg = new TMultiGraph();
         TGraph* g1 = new TGraph(p_dens);
         TGraph* g2 = new TGraph(j_dens);
-//      TGraph* g3 = new TGraph(g_psi);
 
         mg->Add(g1);
         mg->Add(g2);
-//      mg->Add(g3);
         mg->Draw("apl");
         mg->GetXaxis()->SetTitle("x");
         mg->GetYaxis()->SetTitle("y");
-        mg->GetHistogram()->SetTitle("Comparison of probability density with current for a Gaussian (sig=0.1,m=1,t=5)");
+        //mg->GetHistogram()->SetTitle("Comparison of probability density with current for a Gaussian (sig=0.1,m=1,t=5)");
+	mg->GetHistogram()->SetTitle(title_graph.c_str());
 
         TLegend* legend = new TLegend(0.78, 0.695, 0.98, 0.775);
         legend->AddEntry(p_dens, "Probability density", "l");
         legend->AddEntry(j_dens, "Probability current", "l");
-//      legend->AddEntry(g_psi, "Non-relativistic density", "l");
         legend->Draw("Same");
 
 }	

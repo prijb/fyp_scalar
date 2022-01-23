Fixed scaling issue of gaussian.py

gaussian.py
@@ -15,9 +15,10 @@ import matplotlib.pyplot as plt
 x0 = 0          #Gaussian mean position
 x_sig = 1       #Gaussian standard dev in x
 p_sig = 1/x_sig #Gaussian standard dev in p
+m = 1
 
-x = np.linspace(-10,10,100)
-t = 10        #Time at which rho and j are calculated
+x = np.linspace(-10*x_sig,10*x_sig,100)
+t = 5       #Time at which rho and j are calculated
 
 '''
 Additional code to try to make it a 2d colour plot
@@ -46,17 +47,18 @@ def renorm(x,y):
     y = y/norm
     return y
 
-def gaussian(x,x0,sig):
+def gaussian(x,x0,t,sig):
+    #sig = sig*(1 + (t*t/(4*sig*sig)))
     y = (1/np.sqrt(2*np.pi*sig*sig))*np.exp(-(x-x0)**2/(2*sig*sig))
     return y
 
-def integrand_rho(px,py,p0,psig,x,t):
-    i = (1 + 1/(np.sqrt(1+px**2)*np.sqrt(1+py**2)) + px*py/(np.sqrt(1+px**2)*np.sqrt(1+py**2)))*(np.cos((px-py)*x - ((np.sqrt(1+px**2)-np.sqrt(1+py**2))*t)))*np.exp(-(px**2+py**2)/psig**2)
+def integrand_rho(px,py,p0,psig,x,t,m=1):
+    i = (1/(4*np.pi))*(np.sqrt(2/np.pi)*(1/psig))*(1 + m**2/(np.sqrt(m**2+px**2)*np.sqrt(m**2+py**2)) + px*py/(np.sqrt(m**2+px**2)*np.sqrt(m**2+py**2)))*(np.cos((px-py)*x - ((np.sqrt(m**2+px**2)-np.sqrt(m**2+py**2))*t)))*np.exp(-(px**2+py**2)/psig**2)
     
     return i
 
-def integrand_j(px,py,p0,psig,x,t):
-    j = (px/np.sqrt(1+px**2) + py/np.sqrt(1+py**2))*(np.cos((px-py)*x - ((np.sqrt(1+px**2)-np.sqrt(1+py**2))*t)))*np.exp(-(px**2+py**2)/psig**2)
+def integrand_j(px,py,p0,psig,x,t,m=1):
+    j = (1/(4*np.pi))*(np.sqrt(2/np.pi)*(1/psig))*(px/np.sqrt(m**2+px**2) + py/np.sqrt(m**2+py**2))*(np.cos((px-py)*x - ((np.sqrt(m**2+px**2)-np.sqrt(m**2+py**2))*t)))*np.exp(-(px**2+py**2)/psig**2)
     
     return j    
 
@@ -64,11 +66,11 @@ def integrand_j(px,py,p0,psig,x,t):
 Double integrates the integrands above over px,py ranges [-10,10]
 '''
 def rho(x,t):
-    dens = integrate.nquad(integrand_rho,[[-10,10],[-10,10]],args=(0,p_sig,x,t))
+    dens = integrate.nquad(integrand_rho,[[-10*p_sig,10*p_sig],[-10*p_sig,10*p_sig]],args=(0,p_sig,x,t,m))
     return dens[0]
 
 def j(x,t):
-    dens = integrate.nquad(integrand_j,[[-10,10],[-10,10]],args=(0,p_sig,x,t))
+    dens = integrate.nquad(integrand_j,[[-10*p_sig,10*p_sig],[-10*p_sig,10*p_sig]],args=(0,p_sig,x,t,m))
     return dens[0]
 
 '''
@@ -89,15 +91,19 @@ for i,x_in in enumerate(x):
     j_dens[i] = j(x_in,t)
     
 #Rescale rho and j to make sure they're normalised
-p_dens = p_dens/integrate.simpson(p_dens,x)
-j_dens = j_dens/integrate.simpson(p_dens,x)
+p_val = integrate.simpson(p_dens,x)
+print(f'The integrated probability is: {p_val:.3f}')
+
+p_dens = p_dens/p_val
+j_dens = j_dens/p_val
 #%%
 #Plot them against each other
 plt.plot(x,p_dens,color='red',label='Probability density') 
 plt.plot(x,j_dens,color='blue',label='Probability current density')
+plt.plot(x,gaussian(x,x0,t,x_sig),color='green',label='Classical probability density')
 plt.xlabel('Position')
 plt.ylabel(r'$\rho$/j')
-plt.xlim(-10,10)
+plt.xlim(-10*x_sig,10*x_sig)
 plt.grid()
 plt.legend()
 plt.show()   
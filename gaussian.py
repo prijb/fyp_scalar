bdb8f32d0d8f091469820dc22cc92261863bd860
prijith <prijith.b.pradeep@gmail.com>
2022-01-23 14:39:24 +0000

    Fixed scaling issue of gaussian.py

--- gaussian.py
+++ gaussian.py
@@ -15,9 +15,10 @@ import matplotlib.pyplot as plt
 x0 = 0          #Gaussian mean position
 x_sig = 1       #Gaussian standard dev in x
 p_sig = 1/x_sig #Gaussian standard dev in p
+m = 1
 
-x = np.linspace(-10,10,100)
-t = 10        #Time at which rho and j are calculated
+x = np.linspace(-10*x_sig,10*x_sig,100)
+t = 5       #Time at which rho and j are calculated
 
 '''
 Additional code to try to make it a 2d colour plot
@@ -46,17 +47,18 @@ def renorm(x,y):
     y = y/norm
     return y
 
-def gaussian(x,x0,sig):
+def gaussian(x,x0,t,sig):
+    #sig = sig*(1 + (t*t/(4*sig*sig)))
     y = (1/np.sqrt(2*np.pi*sig*sig))*np.exp(-(x-x0)**2/(2*sig*sig))
     return y
 
-def integrand_rho(px,py,p0,psig,x,t):
-    i = (1 + 1/(np.sqrt(1+px**2)*np.sqrt(1+py**2)) + px*py/(np.sqrt(1+px**2)*np.sqrt(1+py**2)))*(np.cos((px-py)*x - ((np.sqrt(1+px**2)-np.sqrt(1+py**2))*t)))*np.exp(-(px**2+py**2)/psig**2)
+def integrand_rho(px,py,p0,psig,x,t,m=1):
+    i = (1/(4*np.pi))*(np.sqrt(2/np.pi)*(1/psig))*(1 + m**2/(np.sqrt(m**2+px**2)*np.sqrt(m**2+py**2)) + px*py/(np.sqrt(m**2+px**2)*np.sqrt(m**2+py**2)))*(np.cos((px-py)*x - ((np.sqrt(m**2+px**2)-np.sqrt(m**2+py**2))*t)))*np.exp(-(px**2+py**2)/psig**2)
     
     return i
 
-def integrand_j(px,py,p0,psig,x,t):
-    j = (px/np.sqrt(1+px**2) + py/np.sqrt(1+py**2))*(np.cos((px-py)*x - ((np.sqrt(1+px**2)-np.sqrt(1+py**2))*t)))*np.exp(-(px**2+py**2)/psig**2)
+def integrand_j(px,py,p0,psig,x,t,m=1):
+    j = (1/(4*np.pi))*(np.sqrt(2/np.pi)*(1/psig))*(px/np.sqrt(m**2+px**2) + py/np.sqrt(m**2+py**2))*(np.cos((px-py)*x - ((np.sqrt(m**2+px**2)-np.sqrt(m**2+py**2))*t)))*np.exp(-(px**2+py**2)/psig**2)
     
     return j    
 
@@ -64,11 +66,11 @@ def integrand_j(px,py,p0,psig,x,t):
 Double integrates the integrands above over px,py ranges [-10,10]
 '''
 def rho(x,t):
-    dens = integrate.nquad(integrand_rho,[[-10,10],[-10,10]],args=(0,p_sig,x,t))
+    dens = integrate.nquad(integrand_rho,[[-10*p_sig,10*p_sig],[-10*p_sig,10*p_sig]],args=(0,p_sig,x,t,m))
     return dens[0]
 
 def j(x,t):
-    dens = integrate.nquad(integrand_j,[[-10,10],[-10,10]],args=(0,p_sig,x,t))
+    dens = integrate.nquad(integrand_j,[[-10*p_sig,10*p_sig],[-10*p_sig,10*p_sig]],args=(0,p_sig,x,t,m))
     return dens[0]
 
 '''
@@ -89,15 +91,19 @@ for i,x_in in enumerate(x):
     j_dens[i] = j(x_in,t)
     
 #Rescale rho and j to make sure they're normalised
-p_dens = p_dens/integrate.simpson(p_dens,x)
-j_dens = j_dens/integrate.simpson(p_dens,x)
+p_val = integrate.simpson(p_dens,x)
+print(f'The integrated probability is: {p_val:.3f}')
+
+p_dens = p_dens/p_val
+j_dens = j_dens/p_val
 #%%
 #Plot them against each other
 plt.plot(x,p_dens,color='red',label='Probability density') 
 plt.plot(x,j_dens,color='blue',label='Probability current density')
+plt.plot(x,gaussian(x,x0,t,x_sig),color='green',label='Classical probability density')
 plt.xlabel('Position')
 plt.ylabel(r'$\rho$/j')
-plt.xlim(-10,10)
+plt.xlim(-10*x_sig,10*x_sig)
 plt.grid()
 plt.legend()
 plt.show()   
--- graph.C
+++ graph.C
@@ -5,51 +5,59 @@ double sig_m = 1.;
 double int_rho(double pos) {
 	double sig = sig_m;
 	double m = 1;
-	double t = 10;
-	TF2* rho = new TF2("rho", "(1 + [3]*[3]/(sqrt([3]*[3] + x*x)*sqrt([3]*[3] + y*y)) + x*y/(sqrt([3]*[3] + x*x)*sqrt([3]*[3] + y*y)))*cos((x-y)*[0] - (sqrt([3]*[3] + x*x) - sqrt([3]*[3] + y*y))*[2])*TMath::Exp(-1.*(x*x + y*y)/([1]*[1])) ", -100*sig, 100*sig, -100*sig, 100*sig);
+	double t = 5;
+	double pi = TMath::Pi();
+	TF2* rho = new TF2("rho", "(0.25/pi)*(sqrt(2./pi)*(1/[1]))*(1 + [3]*[3]/(sqrt([3]*[3] + x*x)*sqrt([3]*[3] + y*y)) + x*y/(sqrt([3]*[3] + x*x)*sqrt([3]*[3] + y*y)))*cos((x-y)*[0] - (sqrt([3]*[3] + x*x) - sqrt([3]*[3] + y*y))*[2])*TMath::Exp(-1.*(x*x + y*y)/([1]*[1])) ", -100*sig, 100*sig, -100*sig, 100*sig);
 	rho->SetParameter(0, pos); //[0] == Position 
 	rho->SetParameter(1, sig); //[1] == Standard deviation in momentum space
 	rho->SetParameter(2, t);   //[2] == Time
 	rho->SetParameter(3, m);   //[3] == Mass 
-	return 0.5*(rho->Integral(-5*sig, 5*sig, -5*sig, 5*sig,1.e-6)); 
+	return (rho->Integral(-5*sig, 5*sig, -5*sig, 5*sig,1.e-5)); 
 }
 
 double int_j(double pos) {
 	double sig = sig_m;
 	double m = 1;
-	double t = 10;
-	TF2* j = new TF2("j", "(x/sqrt([3]*[3] + x*x) + y/sqrt([3]*[3] + y*y))*cos((x-y)*[0] - (sqrt([3]*[3] + x*x) - sqrt([3]*[3] + y*y))*[2])*TMath::Exp(-1.*(x*x + y*y)/([1]*[1])) ", -100*sig, 100*sig, -100*sig, 100*sig);
+	double t = 5;
+	double pi = TMath::Pi();
+	TF2* j = new TF2("j", "(0.25/pi)*(sqrt(2./pi)*(1/[1]))*(x/sqrt([3]*[3] + x*x) + y/sqrt([3]*[3] + y*y))*cos((x-y)*[0] - (sqrt([3]*[3] + x*x) - sqrt([3]*[3] + y*y))*[2])*TMath::Exp(-1.*(x*x + y*y)/([1]*[1])) ", -100*sig, 100*sig, -100*sig, 100*sig);
 	j->SetParameter(0, pos);
 	j->SetParameter(1, sig);
 	j->SetParameter(2, t);
 	j->SetParameter(3, m); 
-	return 0.5*(j->Integral(-5*sig, 5*sig, -5*sig, 5*sig, 1.e-6));
+	return (j->Integral(-5*sig, 5*sig, -5*sig, 5*sig, 1.e-5));
 
 	//(sqrt(2)*sqrt(sqrt(2*pi)*[1]))/(([1]*sqrt([1]))
 }
 
 
-
 void graph() {
 	//Defining rho and j as integrals
-	double t = 10;
+	double t = 5;
 	double sig_0 = (1. / sig_m);
 	//double sig = sig_0 * (1 + t * t / (4*sig_0 * sig_0));
 	double sig = sig_0;
 	double pi = TMath::Pi();
 	double m = 1;
-	TF1* g_rho = new TF1("g_rho", "int_rho(x)/(2*pi)", -10*sig, 10*sig);
-	TF1* g_j = new TF1("g_j", "(int_j(x)/(2*pi))", -10*sig, 10*sig);
-	TF1* g_psi = new TF1("g_psi", "(1/sqrt(2*pi*[0]*[0]))*TMath::Exp(-0.5*x*x/([0]*[0]))", -10*sig, 10*sig);
+	TF1* g_rho = new TF1("g_rho", "int_rho(x)", -10*sig, 10*sig);
+	TF1* g_j = new TF1("g_j", "int_j(x)", -10*sig, 10*sig);
+	TF1* g_psi = new TF1("g_psi", "(1/sqrt(2*pi*[0]*[0]))*TMath::Exp(-0.5*x*x/([0]*[0]))", -20*sig, 20*sig);
 	g_psi->SetParameter(0, sig);
 
 	double rho_int = g_rho->Integral(-10 * sig, 10 * sig, 1.e-3);
 
+//Attempt at faster 1D integration
+/*	Int_t np = 1000;
+	double *x_arr=new double[np];
+       	double *w_arr=new double[np];
+	g_rho->CalcGaussLegendreSamplingPoints(np,x_arr,w_arr,1e-15);
+	double rho_int = g_rho->IntegralFast(np,x_arr,w_arr,-10*sig,10*sig);
+*/
 	//Normalisation check
 	cout << "Integral of calculated density:" << rho_int << endl;
 	cout << "Integral of classical density:" << g_psi->Integral(-10 * sig, 10 * sig, 1.e-3) << endl;
 
-	//Renormalisation of j and rho (Ideally unnecessary, but clearly still needed)
+	//Renormalisation of j and rho (Ideally unnecessary)
 	TF1* g_rho_fin = new TF1("g_rho_fin", "g_rho/[0]", -10 * sig, 10 * sig);
 	TF1* g_j_fin = new TF1("g_j_fin", "g_j/[0]", -10 * sig, 10 * sig);
 	g_rho_fin->SetParameter(0, rho_int);
@@ -75,7 +83,7 @@ void graph() {
 	mg->Draw("apl");
 	mg->GetXaxis()->SetTitle("x");
 	mg->GetYaxis()->SetTitle("y");
-	mg->GetHistogram()->SetTitle("Comparison of probability density with current for a Gaussian (sig=1,m=1,t=10)");
+	mg->GetHistogram()->SetTitle("Comparison of probability density with current for a Gaussian (sig=1,m=1,t=5)");
 
 	TLegend* legend = new TLegend(0.78, 0.695, 0.98, 0.775);
 	legend->AddEntry(g_rho_fin, "Probability density", "l");

fed4dff1a4fa7f30ac0e374c5ac2d9e29fbcead5
prijith <prijith.b.pradeep@gmail.com>
2022-02-05 21:54:41 +0000

    Additional code to split density and to get width

--- graph_int.C
+++ graph_int.C
@@ -53,7 +53,7 @@ double int_rho(double *xvals, double *params){
 		cout << "Integral addition: " << d_intg << endl;
                 */
 	}	
-
+	delete rho;
 	return dp*dp*intg;
 }
 
@@ -110,57 +110,76 @@ double int_j(double *xvals, double *params){
                 cout << "Integral addition: " << d_intg << endl;
                 */
         }
-
+	delete j;
         return dp*dp*intg;
 }
 
 void graph_int(){
 
-	double t = 10.;
+	double t = 0.;
 	double sig_p = 1.;
 	double sig_x = 1.;
 	//double sig_x = 1./sig_p;
 	double m = 1;
-	int n_points = 1000;
+	int n_points = 200;
 
-	double lim = 20*sig_x;
+	double lim = 10*sig_x;
 	
 	TF1* p_dens = new TF1("p_dens",int_rho,-lim,lim,4);
-        TF1* j_dens = new TF1("j_dens",int_j,-lim,lim,4);
+        //TF1* j_dens = new TF1("j_dens",int_j,-lim,lim,4);
+	TF1* p_class = new TF1("p_class","(1/sqrt(2*pi*[0]*[0]*(1 + ([1]/(2*[2]*[0]*[0]))*([1]/(2*[2]*[0]*[0])))))*TMath::Exp(-0.5*x*x/([0]*[0]*(1 + ([1]/(2*[2]*[0]*[0]))*([1]/(2*[2]*[0]*[0])))))",-lim,lim);
 
 	p_dens->SetParameters(t,sig_p,m,n_points);
-	j_dens->SetParameters(t,sig_p,m,n_points);
+	//j_dens->SetParameters(t,sig_p,m,n_points);
+	//p_class->SetParameter(0,sig_x);
+	p_class->SetParameters(1./sig_p,t,m);
 
-	j_dens->SetLineColor(1);
+	//j_dens->SetLineColor(1);
+	p_class->SetLineColor(3);
 
 	double p_int = p_dens->Integral(-lim,lim,1.e-3);
-	double j_int = j_dens->Integral(-lim,lim,1.e-3);
-	
+	double class_int = p_class->Integral(-lim,lim,1.e-3);
+	//double j_int = j_dens->Integral(-lim,lim,1.e-3);
+
+	TH1* dens_hist = p_dens->GetHistogram();
+	dens_hist->SetStats(1);
+
 	std::ostringstream title;
-	title << std::fixed << std::setprecision(2) << "Comparison of probability density with current for a Gaussian (sig=" << 1/sig_p << ",m=" << m << ",t=" << t << ")";
+	title << std::fixed << std::setprecision(2) << "Comparison of classical and relativistic probability density for a Gaussian (sig=" << 1/sig_p << ",m=" << m << ",t=" << t << ")";
+	//title << std::fixed << std::setprecision(2) << "Comparison of probability density with current for a Gaussian (sig=" << 1/sig_p << ",m=" << m << ",t=" << t << ")";
 	std::string title_graph = title.str();	
 	
 	cout << "The integral of probability density is:  " << p_int << endl;
+	cout << "The integral of the classical density is:  " << class_int << endl;
+	//cout << "The standard deviation of the density for sigma: " << 1./sig_p << " is: " << sqrt(p_dens->Variance(-lim,lim)) << endl;
 
-	
+
 	TCanvas* c1 = new TCanvas();
         c1->SetGrid();
 
         TMultiGraph* mg = new TMultiGraph();
         TGraph* g1 = new TGraph(p_dens);
-        TGraph* g2 = new TGraph(j_dens);
+        //TGraph* g2 = new TGraph(j_dens);
+	TGraph* g3 = new TGraph(p_class);
 
         mg->Add(g1);
-        mg->Add(g2);
+        //mg->Add(g2);
+	mg->Add(g3);
         mg->Draw("apl");
+        //mg->Draw("apl");
         mg->GetXaxis()->SetTitle("x");
         mg->GetYaxis()->SetTitle("y");
-        //mg->GetHistogram()->SetTitle("Comparison of probability density with current for a Gaussian (sig=0.1,m=1,t=5)");
 	mg->GetHistogram()->SetTitle(title_graph.c_str());
+
 
         TLegend* legend = new TLegend(0.78, 0.695, 0.98, 0.775);
         legend->AddEntry(p_dens, "Probability density", "l");
-        legend->AddEntry(j_dens, "Probability current", "l");
-        legend->Draw("Same");
+        //legend->AddEntry(j_dens, "Probability current", "l");
+        legend->AddEntry(p_class, "Classical Probabiliy density", "l");
+	legend->Draw("Same"); 
+	
+
+	TCanvas *c2 = new TCanvas();
+	dens_hist->Draw("");
 
 }	
